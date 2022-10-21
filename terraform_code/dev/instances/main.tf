e7272fa045a111046a232bc897f1f848250592b1
Ganishbansal <33124535+Ganishbansal@users.noreply.github.com>
2022-10-21 00:25:39 -0400

    Update main.tf

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -65,7 +65,22 @@ resource "aws_instance" "my_amazon" {
     }
   )
 }
-
+ 
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo yum install docker -y
+    sudo systemctl start docker
+    sudo usermod -a -G docker ec2-user
+    curl -sLo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
+    sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
+    rm -f ./kind
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    rm -f ./kubectl
+    kind create cluster --config kind.yaml​
+  EOF
 
 # Adding SSH key to Amazon EC2
 resource "aws_key_pair" "my_key" {
@@ -190,11 +205,21 @@ resource "aws_eip" "static_eip" {
   )
 }
   # Creating ecr repositories
-  resource "aws_ecr_repository" "foo" {
-  name                 = ["web_app", "my_sql"]
+ resource "aws_ecr_repository" "foo" {
+  name                 = "web_app"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
 }
+    
+ resource "aws_ecr_repository" "foo" {
+  name                 = "my_sql"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
