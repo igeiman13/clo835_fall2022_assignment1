02d95967d95bc76a3bc313aabece53b2b3ec464c
EC2 Default User <ec2-user@ip-172-31-14-143.ec2.internal>
2023-06-04 16:55:56 +0000

    added file

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "green"
+DBPORT = int(os.environ.get("DBPORT")) or "3306"
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
