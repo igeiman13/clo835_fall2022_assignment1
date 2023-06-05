a27cfa98de6f5038a99488d68f5ad7701cadfffe
Namithabejoy <bejoynamitha@gmail.com>
2023-06-05 06:11:57 +0000

    updated gitignore

---
+++ Assignment1/terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Namitha"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "namitha"
+}
