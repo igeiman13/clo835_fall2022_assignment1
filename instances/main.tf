8504dd5bf290056f4a7a1f0d80e99b3cc0c0d05e
tom kelvin mwangi macharia <tmacharia1@myseneca.ca>
2024-02-03 10:05:02 -0500

    adds terraform files

---
+++ instances/main.tf
@@ -0,0 +1,187 @@
+
+
+#----------------------------------------------------------
+# ACS730 - Week 3 - Terraform Introduction
+#
+# Build EC2 Instances
+#
+#----------------------------------------------------------
+
+#  Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Define tags locally
+locals {
+  default_tags = merge(module.globalvars.default_tags, { "env" = var.env })
+  prefix       = module.globalvars.prefix
+  name_prefix  = "${local.prefix}-${var.env}"
+}
+
+# Retrieve global variables from the Terraform module
+module "globalvars" {
+  source = "../../modules/globalvars"
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = var.public_subnet_cidr
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-public-subnet"
+    }
+  )
+}
+
+# Reference subnet provisioned by 01-Networking 
+resource "aws_instance" "my_amazon" {
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = lookup(var.instance_type, var.env)
+  key_name                    = aws_key_pair.my_key.key_name
+  iam_instance_profile   = "LabInstanceProfile"
+  vpc_security_group_ids             = [aws_security_group.my_sg.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+ 
+  associate_public_ip_address = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Amazon-Linux"
+    }
+  )
+}
+
+
+# Adding SSH key to Amazon EC2
+resource "aws_key_pair" "my_key" {
+  key_name   = local.name_prefix
+  public_key = file("${local.name_prefix}.pub")
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  
+  
+  ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Custom port 8080"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    description      = "Custom port 8081"
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Custom port 8082"
+    from_port        = 8082
+    to_port          = 8082
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Custom port 8083"
+    from_port        = 8083
+    to_port          = 8083
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-sg"
+    }
+  )
+}
+
+# Elastic IP
+resource "aws_eip" "static_eip" {
+  instance = aws_instance.my_amazon.id
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-eip"
+    }
+  )
+}
+
+resource "aws_ecr_repository" "database" {
+  name         = "${local.name_prefix}-mysql"
+  tags = {
+    Name = "${local.name_prefix}-mysql"
+  }
+}
+
+resource "aws_ecr_repository" "application" {
+  name         = "${local.name_prefix}-app"
+  tags = {
+    Name = "${local.name_prefix}-app"
+  }
+
+  
+}
